f68ba20c4b18dd6d04bad88eed60244cf7d078a2
sebastiandres <sebastiandres@gmail.com>
2022-02-10 18:57:52 -0300

    improving the about us

--- 00_why.py
+++ 00_why.py
@@ -17,16 +17,17 @@ def display_page():
     """)
 
     if button_placeholder.button("Why?"):
-        #image_placeholder.images("images/mrdna2.gif", width=300)
         image_placeholder.markdown(image_markdown_format.format("mrdna2.gif"))
         markdown_placeholder.markdown("""
         Data Science and Jurassic Park are very similar:
-        * You have to ponder the meaning of life
         * You have to consider not only if you can, but also if you should.
         * It always seems to work great until your first real customer. 
-        * You have to dig for the relevant data and fix it with the most unusual ways. It might look ok but mayhem can (and will) arise later.
+        * You have to dig for the relevant data and patch it in the most unusual ways. It might look ok but mayhem can (and will) arise later.
         * Your creations will likely bite you back (?).
         * It was all good until lawyers and business man started making decisions.
         """)
         button_placeholder.empty()
-        st.markdown("Use the sidebar to go through this interactive Streamlit app to learn about the ellusive Datasaurus.")
+
+        st.markdown("Join the quest to learn about the ellusive Datasaurus.)
+
+        st.markdown("Use the sidebar to navigate through this interactive Streamlit app")
--- 05_about.py
+++ 05_about.py
@@ -4,31 +4,38 @@ def display_page():
     # Set title
     st.title("About")
 
+    # Columns
+    c1, c2  = st.columns([2,8])
+
+    # Image
+    image_markdown_format = "![Alt Text](https://github.com/sebastiandres/streamlit_datasaurus/blob/main/images/{}?raw=true)"
+    c1.markdown(image_markdown_format.format("mrdna.png"))
+
     # Authors
-    st.header("The authors")
-    st.markdown("""
+    c2.header("The authors")
+    c2.markdown("""
     * Diego Rojo:\t\t[website](https://www.93degree.me/) - [github](https://github.com/93degree) - [linkedin](https://www.linkedin.com/in/diego-rojo/) - [twitter](https://twitter.com/Diego_Rojo_)
     * Sebastián Flores:\t\t[website](https://sebastiandres.github.io/blog/) - [github](https://github.com/sebastiandres) - [linkedin](https://www.linkedin.com/in/sebastiandres/) - [twitter](https://twitter.com/sebastiandres)""")
 
     # Anscombe
-    st.header("The Anscombe Dataset")
-    st.markdown("""
+    c2.header("The Anscombe Dataset")
+    c2.markdown("""
     * [Original publication by Francis Anscombe on 1973](https://www.tandfonline.com/doi/abs/10.1080/00031305.1973.10478966)
     * [Wikipedia's entry on Anscombe's Quarter](https://en.wikipedia.org/wiki/Anscombe%27s_quartet)
     """)    
 
 
     # Datasaurius
-    st.header("The Datasaurus")
-    st.markdown("""
-    * [Tweet that originated the madness](https://twitter.com/sebastiandres/status/1209898410083717120)
+    c2.header("The Datasaurus")
+    c2.markdown("""
+    * [The tweet that originated the madness](https://twitter.com/albertocairo/status/765167969139765250) by Alberto Cairo.
     * [Same Stats, Different Graphs - also known as the Datasaurus paper](https://www.autodesk.com/research/publications/same-stats-different-graphs) by Justin Matejka and George Fitzmaurice.
     * [Python implementation of Same Stats, Different Graphs](https://github.com/jmatejka/same-stats-different-graphs) by jmatejka.
     """)
 
     # Other links and references
-    st.header("Links and references")
-    st.markdown("""
+    c2.header("Links and references")
+    c2.markdown("""
     * All characters from Jurassic Park's franchise have been used in good faith with no comercial intention.
     * The gifs: made with [youtube](https://www.youtube.com/watch?v=h58lRIVHhGc) and [gifrun](https://gifrun.com/).
     * [Dinosaur Supervisor Phil Tippett](https://github.com/sebastiandres/streamlit_datasaurus/blob/main/images/phil.jpg?raw=true). You had one job, Phil one job.
